dec9eccbaa44f989e760fc3c533e5fc577a7b88f
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2023-08-26 16:23:08 -0500

    Update UsuarioDAO.java

--- src/main/java/modelo/dao/UsuarioDAO.java
+++ src/main/java/modelo/dao/UsuarioDAO.java
@@ -3,6 +3,7 @@ package modelo.dao;
 import modelo.entidades.Usuario;
 
 public interface UsuarioDAO extends GenericDAO<Usuario, Integer> {
-	public Usuario autorizar(String nombre, String clave);
 
+	public Usuario autorizar(String nombre, String clave);
+	public Usuario validarUsuarioParaRegistrar(String nombre, String clave);
 }
