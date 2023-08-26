7f63b7502cfc4e765ed52b861ff15512096cbf20
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2023-08-26 14:13:06 -0500

    Created LoginController

---
+++ src/main/java/controlador/LoginController.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+
+    public LoginController() {
+        super();
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ruteador(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ruteador(request, response);
+	}
+	
+	private void ruteador(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String ruta = (request.getParameter("ruta")==null?"ver":request.getParameter("ruta"));
+		
+		switch(ruta) {
+		case "ver":
+			inicio(request, response);
+			break;
+		}
+	}
+
+	private void inicio(HttpServletRequest request, HttpServletResponse response) {
+	}
+
+
+}
