43d71ca67e53afe49cf570d070e77e90232d1b40
SLeonCamacho <74388091+SLeonCamacho@users.noreply.github.com>
2023-09-01 12:22:58 -0500

    qwfesf

---
+++ src/main/java/modelo/jpa/UsuarioRecurso.java
@@ -0,0 +1,41 @@
+package modelo.jpa;
+
+//package serviciosrest;
+
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import modelo.dao.DAOFactory;
+import modelo.dao.UsuarioDAO;
+import modelo.entidades.Usuario;
+
+
+@Path("/usuarios")
+public class UsuarioRecurso {
+	
+	@POST
+    @Path("/registrar")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void guardarUsuario(@QueryParam("nombre") String nombre, @QueryParam("clave") String clave) {
+        
+        UsuarioDAO usuarioDAO = DAOFactory.getFactory().getUsuarioDAO();
+		Usuario usuarioExistente = usuarioDAO.validarUsuarioParaRegistrar(nombre, clave);
+
+		if (usuarioExistente != null) {
+			usuarioDAO.create(usuarioExistente);
+		} else {
+			System.out.println("Usuario ya registrado.");
+		}
+    }
+	
+}
